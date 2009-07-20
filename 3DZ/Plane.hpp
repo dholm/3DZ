a9b643e08b906bad5cca6dbf857b4b4e0811ea3d
David Holm <dholm@david-holms-macbook-pro.local>
2009-07-20 12:32:31 +0200

    TDZ: Plane is now a template

--- 3DZ/Plane.hpp
+++ 3DZ/Plane.hpp
@@ -10,17 +10,47 @@
 #ifndef TDZ_PLANE_HPP
 #define TDZ_PLANE_HPP
 
-#include <3DZ/Mesh.hpp>
+#include <3DZ/Vector.hpp>
 
 namespace TDZ {
 	
+	template <typename ComponentT>
 	class Plane {
 	public:
-		Plane(const Mesh::Vertex& normal, float distance);
+		Plane() :
+			m_normal(Vector3<ComponentT>(0, 0, 1)),
+			m_distance(ComponentT(0))
+		{
+		}
+		
+		Plane(const Vector<3, ComponentT>& normal, ComponentT distance) :
+			m_normal(normal),
+			m_distance(distance)
+		{
+		}
+		
+		Plane(const Vector<3, ComponentT>& normal, const Vector<3, ComponentT>& point) :
+			m_normal(normal),
+			m_distance(normal.dot(point))
+		{
+		}
+		
+		const Vector<3, ComponentT>& normal() const {
+			return m_normal;
+		}
+		
+		const ComponentT& distance() const {
+			return m_distance;
+		}
+		
+		Plane<ComponentT> normalized() const {
+			ComponentT m = ComponentT(1) / m_normal.length();
+			return Plane<ComponentT>(Vector3<ComponentT>(m_normal[0] * m, m_normal[1] * m, m_normal[2] * m), m_distance * m);
+		}
 		
 	private:
-		Mesh::Vertex m_normal;
-		float m_distance;
+		Vector<3, ComponentT> m_normal;
+		ComponentT m_distance;
 	};
 	
 } // TDZ
